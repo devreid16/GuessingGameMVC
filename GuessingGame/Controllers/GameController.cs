96d23c1ed354c512c4c566a512854547a33a1207
Angela Reid <reidang94@gmail.com>
2016-07-19 19:34:34 -0400

    add Guessing Game MVC App

---
+++ GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuessingGame.Models;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 10);
+
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess)
+        {
+            return guess == (int )Session["Answer"];
+        }
+
+        [HttpPost]  //attribute that decorates index overload
+        [ValidateAntiForgeryToken] //extra layer of security, not authentication
+        
+        public ActionResult Index(GameModel model)
+        {           
+            if (ModelState.IsValid) 
+            {
+                ViewBag.Win = GuessWasCorrect(model.Guess);  
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    //data model for our page controller will pull this and give to view
+    //required gives consistent validation; tells system to create instance of required
+    public class GameModel
+    {
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage = "You must enter your name")]
+        [MinLength(2,ErrorMessage = "Name is too short")] 
+        [MaxLength(20, ErrorMessage = "Name is too long")]
+        
+        public string PlayerName { get; set; }
+     
+
+        [Display(Name = "What is your guess?")]        
+        [Required(ErrorMessage = "Guess is required")]
+        [Range(1, 10, ErrorMessage = "Guess must be between 1 and 10")]
+
+        public int Guess { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PlayerName} ({Guess})";
+        }
+    }
+}
